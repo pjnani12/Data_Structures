a5a94956166f57db4551609ecc8defd3a58e0100
JAGADEESH <pjnani12@gmail.com>
2021-06-13 00:48:45 +0530

    Update deepcopy.cpp

--- deepcopy.cpp
+++ deepcopy.cpp
@@ -1,37 +1,35 @@
 #include <iostream>
 using namespace std;
 class marks{
-	int* total;
-public:
-	marks(){
-		total = new int;
-	}
-	void set_val(int tot){
-		*total = tot;
-	}
-	void show_data(){
-		cout<< "\n total = " << *total<< endl;
-	}
-	// Copy Constructors for implementing deep copy
-	marks(marks& sample){
-		total = new int;
-		*total = *(sample.total);
-	}
-	~marks(){
-		delete total;
-	}
+    int *total;
+    public:
+    marks(){
+        total=new int;
+    }
+    marks(marks &obj){//copy constructor for implementing deep copy
+        total=new int;
+        *total=*(obj.total);
+    }
+    void set_val(int a){
+        *total=a;
+    }
+    ~marks(){
+        delete total;
+    }
+    void print(){
+        cout<<*total<<endl;
+    }
 };
-int main(){
-	marks first;
-	first.set_val(14);
-	first.show_data();
-	// When the data will be copied then
-	// all the resources will also get
-	// allocated to the new object
-	marks second = first;
-	second.set_val(18);
-	// Display the dimensions
-	second.show_data();
-
-	return 0;
+int main() { 
+     marks m;
+     m.set_val(10);
+     m.print();
+     marks j=m;//deep copy , 2 addresses are allocated separatly
+     cout<<"j value";j.print();
+     cout<<"m value";m.print();
+     cout<<"changing only j value to 99"<<endl;
+     j.set_val(99);
+     cout<<"j value";j.print();
+     cout<<"m value";m.print();
+     return 0;
 }
