855523b1477a636bbb7410d687dd53b551a37bc9
JAGADEESH <pjnani12@gmail.com>
2021-06-17 01:12:11 +0530

    Update strings.cpp

--- strings.cpp
+++ strings.cpp
@@ -11,10 +11,19 @@ string removeStr(char *s,char rmv){
     }
     return rtn;
 }
+string reverse(char *s){
+    string rev="";
+    int n=strlen(s);
+    for(int i=n-1;i>=0;i--){
+        rev=rev+s[i];
+    }
+    return rev;
+}
 
 int main() {
     char str[]="jagadeesh";
-    string rtn=removeStr(str,'e');
+    //string rtn=removeStr(str,'e');
+    string rtn=reverse(str);
     cout<<rtn;
 	return 0;
 }
