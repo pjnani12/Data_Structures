9e2fd024d98e02d61f23d37a98ea88fa9687ba79
JAGADEESH <pjnani12@gmail.com>
2021-06-25 12:04:21 +0530

    Create smallest_balancedString.cpp

---
+++ smallest_balancedString.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+bool balanced(int small[], int caps[]){
+    for (int i = 0; i < 26; i++) {
+        if (small[i] != 0 && (caps[i] == 0)){
+            return 0;
+        }
+        else if ((small[i] == 0) && (caps[i] != 0)){
+            return 0;
+        }
+    }
+    return 1;
+}
+void subString(string s, int n){
+    int smallSize=999999999;
+    string ans;
+	for (int i = 0; i < n; i++){
+	    int small[26]={0};
+        int caps[26]={0};
+		for (int len = 1; len <= n - i; len++){
+			string str=s.substr(i, len);
+			for (int i = 0; i < str.length(); i++) {
+                 if (str[i] >= 65 && str[i] <= 90)
+                    caps[str[i] - 'A']++;
+                else
+                    small[str[i] - 'a']++;
+            }
+            balanced(small,caps)?cout<<str<<" ":cout<<"";
+            if(balanced(small,caps)){
+                if(smallSize>(str.length())){
+                    smallSize=str.length();
+                    ans=str;
+                }
+            }
+		}
+	}
+	cout<<endl<<"answer is:: "<<ans<<endl;
+	
+}
+
+int main(){
+	string s = "azABaabba";
+	subString(s,s.length());
+	return 0;
+}
